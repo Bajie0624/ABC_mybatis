fd9915bf5c94cbbf3a7f58b4d168356f18063066
username <zhangxuwei@Bajie.local>
2020-07-29 22:57:42 +0800

    增加一对多和多对一代码

---
+++ day03/day03_02/src/main/java/com/mybatis/domain/Account.java
@@ -0,0 +1,54 @@
+package com.mybatis.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author Bajie
+ * @date 2020/7/28 8:41 下午
+ */
+public class Account implements Serializable {
+    private Integer id;
+    private Integer uid;
+    private double money;
+    private User user;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", uid=" + uid +
+                ", money=" + money +
+                '}';
+    }
+}
---
+++ day03/day03_02/src/main/java/com/mybatis/domain/User.java
@@ -0,0 +1,80 @@
+package com.mybatis.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Bajie
+ * @date 2020/7/24 9:56 下午
+ */
+public class User implements Serializable {
+
+    private Integer id;
+    private String username;
+    private Date birthday;
+    private String sex;
+    private String address;
+    //一对多关系映射，主表实体类中应该包含从表实体的引用集合
+
+    private List<Account> accounts;
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", birthday=" + birthday +
+                ", sex='" + sex + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+}
